da863511aea736203f2dfb4d19f929b47f08c6d5
jessamelie <jessamelie.sola@gmail.com>
2023-06-12 17:07:46 +0200

    CONFIG: setting up moongose

--- tinderClone_back/server.js
+++ tinderClone_back/server.js
@@ -5,15 +5,38 @@ import dotenv from "dotenv";
 //App Config
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8001
-
 
 //Middlewares 
 
 //DB config
+mongoose.connect(process.env.CONNECTION_URL)
 
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World!'));
 
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err,data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    });
+});
+
+app.get('/tinder/card', (req, res) => {
+
+    Cards.find((err,data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    });
+});
+
+
 //Listener
-app.listen(port, () => console.log(`listening on localhost: ${port}`))
+app.listen(process.env.PORT, () => console.log(`listening on localhost: ${process.env.PORT}`))
